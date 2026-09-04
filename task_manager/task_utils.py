2854c4b3b54f82f4f2e1f054eb8f85d98cf6159b
monte <williammonte3592@gmail.com>
2026-09-04 12:27:55 +0300

    define task list and implement add_task function while validating inputs

--- task_manager/task_utils.py
+++ task_manager/task_utils.py
@@ -1,2 +1,18 @@
 from datetime import datetime
 from validation import validate_task_title, validate_task_description, validate_due_date
+
+tasks = []
+
+def add_task(title, description, due_date):
+
+    valid_title, title_msg = validate_task_title(title)
+    if not valid_title:
+        return False, title_msg
+    
+    valid_desc, desc_msg = validate_task_description(description)
+    if not valid_desc:
+        return False, desc_msg
+    
+    valid_date, date_msg = validate_due_date(due_date)
+    if not valid_date:
+        return False, date_msg
